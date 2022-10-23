be23fbd0268515a1f4f3b6a2f076e1c7f88bc21b
Stanislav Efimov <61292878+Stanpie3@users.noreply.github.com>
2022-10-24 01:19:54 +0300

    Add files via upload

--- branch_and_bound.cpp
+++ branch_and_bound.cpp
@@ -1,4 +1,14 @@
 #include "branch_and_bound.h"
+#include<iostream>
+
+template <class T>
+void print_vector(const vector<T> v) {
+    cout << "\n";
+    for (const auto c : v) {
+        cout << c << " ";
+    }
+    cout << "\n";
+}
 
 bool operator == (const Node& lhs, const Node& rhs) {
     return lhs.number == rhs.number;
@@ -15,6 +25,19 @@ void remove_a_from_b(const set<int>& A, vector<int>& b) {
     }
 }
 
+bool test_feasible(const vector<float> sol, const vector<vector<int>>& A, const vector<int>& b) {
+    for (int j = 0; j < A.size(); j++) {
+        int rest = 0;
+        for (int i = 0; i < sol.size(); i++) {
+            if (sol[i] > 0) {
+                rest += A[j][i];
+            }
+            if (rest > b[j]) { return false; }
+        }
+    }
+    return true;
+}
+
 void bound_for_node(Node& node, const vector<vector<int>>& Subscripts,
     const vector<int>& v, const vector<vector<int>>& A, const vector<int>& b) {
     vector<float> Z;            //Задаём вектора Z и bound_items - в них будут отходить границы и граничные элементы каждой операции
@@ -26,7 +49,7 @@ void bound_for_node(Node& node, const vector<vector<int>>& Subscripts,
         float sum = 0;
         float z = 0;
         for (auto c : node.I) {             //Заранее подсчитываем влияние на сумму и границу фиксированных в 1 элементов
-            sum += A[j][c];
+            sum += A[j][c];                 //Тут ошибка?
             z += v[c];
             Solutions[j][c] = 1.0;
         }
@@ -52,31 +75,35 @@ void bound_for_node(Node& node, const vector<vector<int>>& Subscripts,
                 else if (sum + A[j][*i] == b[j]) { //В случае равенства мы получили достижимую границу, отмечаем это
                     z += v[*i];
                     Solutions[j][*i] = 1.0;
-                    bound_items.push_back(-1);  // Показатель достижимости (проходим по ограничению, так как в точности достигли раницы)
+                    bound_items.push_back(Idx.back());  // Изменено
                     break;
                 }
                 else {
                     coef = (b[j] - sum) / A[j][*i];       //Находим коэффициент для границы из ограничения
                     Solutions[j][*i] = coef;
-                    bound_items.push_back(*i);            //Добавляем граничный элемент и его вес с коэффицентом к оцениваемой функции
+                    bound_items.push_back(Idx.back());            //Добавляем граничный элемент и его вес с коэффицентом к оцениваемой функции //Изменено
                     z += v[*i] * coef;
                     break;
                 }
             }
             else {
-                bound_items.push_back(-1);
+                if (test_feasible(Solutions[j], A, b)) {
+                    bound_items.push_back(-1);
+                }
+                else {
+                    bound_items.push_back(-2);
+                }
                 break;
             }
         }
         Z.push_back(z);         //Пушим в вектор z каждую промежуточную границу 
     }
-    /*for (auto z : Z) {
-        cout << z << " ";
-    }
-    cout << endl;*/
     auto it = min_element(Z.begin(), Z.end());          //Находим минимум среди границ, берем соответствующий ей граничный элемент
     node.Bound = *it;
     node.bound_item_index = bound_items.at(distance(Z.begin(), it));
+    if (node.bound_item_index == -2) {
+        node.Bound = -9999;
+    }
     node.solution = Solutions.at(distance(Z.begin(), it));
 }
 
